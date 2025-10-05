7dab900c43f6e5dc819c0ac6bf72f43cfe281252
516 Hackers <125684516+516hackers@users.noreply.github.com>
2025-10-06 00:12:49 +0500

    Update __init__.py

--- src/pakistan_validator/__init__.py
+++ src/pakistan_validator/__init__.py
@@ -3,5 +3,11 @@ Pakistan Digital Validator
 Ethical toolkit for CNIC and phone number validation
 """
 
+from .cnic_validator import CNICValidator
+from .phone_validator import PhoneValidator
+from .synthetic_cnic import SyntheticCNICGenerator
+from .cnic_ocr import CNICOCR
+
 __version__ = "1.0.0"
 __author__ = "516 Hackers"
+__all__ = ['CNICValidator', 'PhoneValidator', 'SyntheticCNICGenerator', 'CNICOCR']
