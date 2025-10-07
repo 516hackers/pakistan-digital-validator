9164a0a283823752a0ebd8168ae2cbc5c0a0e5f0
516 Hackers <125684516+516hackers@users.noreply.github.com>
2025-10-07 23:29:56 +0500

    Update cnic_validator.py

--- src/pakistan_validator/cnic_validator.py
+++ src/pakistan_validator/cnic_validator.py
@@ -14,158 +14,64 @@ class CNICValidator:
     def __init__(self):
         self.region_codes = self._load_region_codes()
         self.division_codes = self._load_division_codes()
-        self.district_codes = self._load_district_codes()
         self.exact_district_mapping = self._load_exact_district_mapping()
         self.cnic_pattern = re.compile(r'^\d{5}-\d{7}-\d{1}$')
         self.digits_pattern = re.compile(r'^\d{13}$')
     
     def _load_region_codes(self) -> Dict[str, str]:
         """Load region codes from JSON file"""
-        return {
-            '1': 'Khyber Pakhtunkhwa', 
-            '2': 'FATA', 
-            '3': 'Punjab', 
-            '4': 'Sindh', 
-            '5': 'Balochistan', 
-            '6': 'Islamabad', 
-            '7': 'Gilgit-Baltistan',
-            '8': 'Azad Jammu & Kashmir'
-        }
+        data_path = os.path.join(os.path.dirname(__file__), 'data', 'regions.json')
+        try:
+            with open(data_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                return data.get('region_codes', {})
+        except:
+            return {
+                '1': 'Khyber Pakhtunkhwa', 
+                '2': 'FATA', 
+                '3': 'Punjab', 
+                '4': 'Sindh', 
+                '5': 'Balochistan', 
+                '6': 'Islamabad', 
+                '7': 'Gilgit-Baltistan',
+                '8': 'Azad Jammu & Kashmir'
+            }
     
     def _load_division_codes(self) -> Dict[str, str]:
-        """Load NADRA division codes based on first 2 digits after province code"""
-        return {
-            # Khyber Pakhtunkhwa (1XXXX)
-            "11": "Bannu Division", "12": "Dera Ismail Khan Division", "13": "Hazara Division",
-            "14": "Kohat Division", "15": "Malakand Division", "16": "Mardan Division", "17": "Peshawar Division",
-            
-            # FATA (2XXXX)
-            "21": "FATA Region",
-            
-            # Punjab (3XXXX) - CORRECTED MAPPING
-            "31": "Bahawalpur Division",  # Includes Bahawalpur, Bahawalnagar, Rahim Yar Khan
-            "32": "Dera Ghazi Khan Division",
-            "33": "Faisalabad Division", 
-            "34": "Gujranwala Division",
-            "35": "Lahore Division",
-            "36": "Multan Division",
-            "37": "Rawalpindi Division",
-            "38": "Sargodha Division",
-            
-            # Sindh (4XXXX)
-            "41": "Hyderabad Division", "42": "Karachi Division", "43": "Larkana Division",
-            "44": "Mirpur Khas Division", "45": "Sukkur Division", "46": "Shaheed Benazirabad Division",
-            
-            # Balochistan (5XXXX)
-            "51": "Kalat Division", "52": "Makran Division", "53": "Nasirabad Division",
-            "54": "Quetta Division", "55": "Sibi Division", "56": "Zhob Division",
-            
-            # Islamabad (6XXXX)
-            "61": "Islamabad",
-            
-            # Gilgit-Baltistan (7XXXX)
-            "71": "Gilgit Division",
-            
-            # Azad Jammu & Kashmir (8XXXX)
-            "81": "Mirpur Division", "82": "Muzaffarabad Division"
-        }
-    
-    def _load_district_codes(self) -> Dict[str, Dict]:
-        """Load complete district mapping within divisions"""
-        return {
-            # Khyber Pakhtunkhwa
-            "11": {"Bannu": "Bannu", "Lakki Marwat": "Lakki Marwat", "North Waziristan": "North Waziristan"},
-            "12": {"Dera Ismail Khan": "Dera Ismail Khan", "Tank": "Tank", "South Waziristan": "South Waziristan"},
-            "13": {"Abbottabad": "Abbottabad", "Haripur": "Haripur", "Mansehra": "Mansehra", "Batagram": "Batagram", "Kohistan": "Kohistan", "Tor Ghar": "Tor Ghar"},
-            "14": {"Kohat": "Kohat", "Karak": "Karak", "Hangu": "Hangu", "Kurram": "Kurram", "Orakzai": "Orakzai"},
-            "15": {"Malakand": "Malakand", "Swat": "Swat", "Buner": "Buner", "Shangla": "Shangla", "Lower Dir": "Lower Dir", "Upper Dir": "Upper Dir", "Chitral": "Chitral"},
-            "16": {"Mardan": "Mardan", "Swabi": "Swabi"},
-            "17": {"Peshawar": "Peshawar", "Charsadda": "Charsadda", "Nowshera": "Nowshera"},
-            
-            # FATA
-            "21": {"Bajaur": "Bajaur", "Mohmand": "Mohmand", "Khyber": "Khyber", "FR Peshawar": "FR Peshawar"},
-            
-            # Punjab - CORRECTED
-            "31": {"Bahawalpur": "Bahawalpur", "Bahawalnagar": "Bahawalnagar", "Rahim Yar Khan": "Rahim Yar Khan"},
-            "32": {"Dera Ghazi Khan": "Dera Ghazi Khan", "Layyah": "Layyah", "Muzaffargarh": "Muzaffargarh", "Rajanpur": "Rajanpur"},
-            "33": {"Faisalabad": "Faisalabad", "Chiniot": "Chiniot", "Jhang": "Jhang", "Toba Tek Singh": "Toba Tek Singh"},
-            "34": {"Gujranwala": "Gujranwala", "Gujrat": "Gujrat", "Hafizabad": "Hafizabad", "Mandi Bahauddin": "Mandi Bahauddin", "Narowal": "Narowal", "Sialkot": "Sialkot"},
-            "35": {"Lahore": "Lahore", "Kasur": "Kasur", "Nankana Sahib": "Nankana Sahib", "Sheikhupura": "Sheikhupura"},
-            "36": {"Multan": "Multan", "Khanewal": "Khanewal", "Lodhran": "Lodhran", "Vehari": "Vehari", "Sahiwal": "Sahiwal", "Pakpattan": "Pakpattan"},
-            "37": {"Rawalpindi": "Rawalpindi", "Attock": "Attock", "Chakwal": "Chakwal", "Jhelum": "Jhelum", "Talagang": "Talagang", "Murree": "Murree"},
-            "38": {"Sargodha": "Sargodha", "Bhakkar": "Bhakkar", "Khushab": "Khushab", "Mianwali": "Mianwali"},
-            
-            # Sindh
-            "41": {"Hyderabad": "Hyderabad", "Badin": "Badin", "Dadu": "Dadu", "Jamshoro": "Jamshoro", "Matiari": "Matiari", "Sujawal": "Sujawal", "Tando Allahyar": "Tando Allahyar", "Tando Muhammad Khan": "Tando Muhammad Khan", "Tharparkar": "Tharparkar", "Thatta": "Thatta"},
-            "42": {"Karachi Central": "Karachi Central", "Karachi East": "Karachi East", "Karachi South": "Karachi South", "Karachi West": "Karachi West", "Korangi": "Korangi", "Malir": "Malir"},
-            "43": {"Larkana": "Larkana", "Jacobabad": "Jacobabad", "Kashmore": "Kashmore", "Qambar Shahdadkot": "Qambar Shahdadkot", "Shikarpur": "Shikarpur"},
-            "44": {"Mirpur Khas": "Mirpur Khas", "Umerkot": "Umerkot", "Tharparkar": "Tharparkar"},
-            "45": {"Sukkur": "Sukkur", "Ghotki": "Ghotki", "Khairpur": "Khairpur"},
-            "46": {"Shaheed Benazirabad": "Shaheed Benazirabad", "Sanghar": "Sanghar", "Naushahro Feroze": "Naushahro Feroze"},
-            
-            # Balochistan
-            "51": {"Kalat": "Kalat", "Khuzdar": "Khuzdar", "Mastung": "Mastung", "Awaran": "Awaran", "Lasbela": "Lasbela", "Kharan": "Kharan", "Washuk": "Washuk"},
-            "52": {"Kech": "Kech", "Gwadar": "Gwadar", "Panjgur": "Panjgur"},
-            "53": {"Nasirabad": "Nasirabad", "Jafarabad": "Jafarabad", "Jhal Magsi": "Jhal Magsi", "Kachhi": "Kachhi", "Lehri": "Lehri", "Sohbatpur": "Sohbatpur"},
-            "54": {"Quetta": "Quetta", "Pishin": "Pishin", "Killa Abdullah": "Killa Abdullah"},
-            "55": {"Sibi": "Sibi", "Dera Bugti": "Dera Bugti", "Kohlu": "Kohlu", "Ziarat": "Ziarat", "Harnai": "Harnai"},
-            "56": {"Zhob": "Zhob", "Barkhan": "Barkhan", "Killa Saifullah": "Killa Saifullah", "Loralai": "Loralai", "Musakhel": "Musakhel", "Sherani": "Sherani"},
-            
-            # Islamabad
-            "61": {"Islamabad": "Islamabad"},
-            
-            # Gilgit-Baltistan
-            "71": {"Gilgit": "Gilgit", "Skardu": "Skardu", "Ghanche": "Ghanche", "Astore": "Astore", "Diamer": "Diamer", "Ghizer": "Ghizer", "Hunza": "Hunza", "Nagar": "Nagar", "Kharmang": "Kharmang", "Shigar": "Shigar"},
-            
-            # Azad Jammu & Kashmir
-            "81": {"Mirpur": "Mirpur", "Bhimber": "Bhimber", "Kotli": "Kotli"},
-            "82": {"Muzaffarabad": "Muzaffarabad", "Hattian": "Hattian", "Neelum": "Neelum", "Poonch": "Poonch", "Bagh": "Bagh", "Haveli": "Haveli", "Sudhnati": "Sudhnati"}
-        }
+        """Load division codes from JSON file"""
+        data_path = os.path.join(os.path.dirname(__file__), 'data', 'regions.json')
+        try:
+            with open(data_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                return data.get('division_codes', {})
+        except:
+            return {
+                "11": "Bannu Division", "12": "Dera Ismail Khan Division", "13": "Hazara Division",
+                "14": "Kohat Division", "15": "Malakand Division", "16": "Mardan Division", "17": "Peshawar Division",
+                "21": "FATA Region",
+                "31": "Bahawalpur Division", "32": "Dera Ghazi Khan Division", "33": "Faisalabad Division",
+                "34": "Gujranwala Division", "35": "Lahore Division", "36": "Multan Division",
+                "37": "Rawalpindi Division", "38": "Sargodha Division",
+                "41": "Hyderabad Division", "42": "Karachi Division", "43": "Larkana Division",
+                "44": "Mirpur Khas Division", "45": "Sukkur Division", "46": "Shaheed Benazirabad Division",
+                "51": "Kalat Division", "52": "Makran Division", "53": "Nasirabad Division",
+                "54": "Quetta Division", "55": "Sibi Division", "56": "Zhob Division",
+                "61": "Islamabad", "71": "Gilgit Division", "81": "Mirpur Division", "82": "Muzaffarabad Division"
+            }
     
     def _load_exact_district_mapping(self) -> Dict[str, str]:
-        """Load exact district mapping based on first 5 digits"""
-        return {
-            # Bahawalpur Division (31XXX)
-            "31101": "Bahawalpur", "31102": "Bahawalpur", "31103": "Bahawalpur",
-            "31201": "Bahawalnagar", "31202": "Bahawalnagar", "31203": "Bahawalnagar", 
-            "31301": "Rahim Yar Khan", "31302": "Rahim Yar Khan", "31303": "Rahim Yar Khan", "31304": "Rahim Yar Khan",
-            
-            # Lahore Division (35XXX)
-            "35101": "Lahore", "35102": "Lahore", "35103": "Lahore",
-            "35201": "Kasur", "35202": "Kasur", "35203": "Kasur",
-            "35301": "Nankana Sahib", "35302": "Nankana Sahib",
-            "35401": "Sheikhupura", "35402": "Sheikhupura",
-            
-            # Rawalpindi Division (37XXX)
-            "37101": "Rawalpindi", "37102": "Rawalpindi",
-            "37201": "Attock", "37202": "Attock",
-            "37301": "Chakwal", "37302": "Chakwal",
-            "37401": "Jhelum", "37402": "Jhelum",
-            
-            # Faisalabad Division (33XXX)
-            "33101": "Faisalabad", "33102": "Faisalabad",
-            "33201": "Chiniot", "33202": "Chiniot",
-            "33301": "Jhang", "33302": "Jhang",
-            "33401": "Toba Tek Singh", "33402": "Toba Tek Singh",
-            
-            # Multan Division (36XXX)
-            "36101": "Multan", "36102": "Multan",
-            "36201": "Khanewal", "36202": "Khanewal",
-            "36301": "Lodhran", "36302": "Lodhran",
-            "36401": "Vehari", "36402": "Vehari",
-            "36501": "Sahiwal", "36502": "Sahiwal",
-            "36601": "Pakpattan", "36602": "Pakpattan",
-            
-            # Gujranwala Division (34XXX)
-            "34101": "Gujranwala", "34102": "Gujranwala",
-            "34201": "Gujrat", "34202": "Gujrat",
-            "34301": "Hafizabad", "34302": "Hafizabad",
-            "34401": "Mandi Bahauddin", "34402": "Mandi Bahauddin",
-            "34501": "Narowal", "34502": "Narowal",
-            "34601": "Sialkot", "34602": "Sialkot",
-            
-            # Add more exact mappings as verified
-        }
+        """Load exact district mapping from JSON file"""
+        data_path = os.path.join(os.path.dirname(__file__), 'data', 'regions.json')
+        try:
+            with open(data_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                return data.get('exact_district_mapping', {})
+        except:
+            return {
+                "31304": "Rahim Yar Khan",
+                "35101": "Lahore",
+                "37101": "Rawalpindi"
+            }
     
     def validate_format(self, cnic: str) -> bool:
         """
@@ -224,7 +130,7 @@ class CNICValidator:
             'division_code': division_code,
             'division': self._get_division_name(division_code),
             'district_code': district_code,
-            'district': self._get_exact_district_name(full_district_code, division_code, district_code),
+            'district': self._get_exact_district_name(full_district_code),
             'gender': "Male" if gender_digit % 2 == 1 else "Female",
             'gender_digit': gender_digit,
             'unique_number': cleaned[5:12]  # Remaining unique digits
@@ -234,20 +140,9 @@ class CNICValidator:
         """Get division name from division code"""
         return self.division_codes.get(division_code, f"Division Code: {division_code}")
     
-    def _get_exact_district_name(self, full_district_code: str, division_code: str, district_code: str) -> str:
-        """Get exact district name using full 5-digit code"""
-        # Try exact mapping first
-        if full_district_code in self.exact_district_mapping:
-            return self.exact_district_mapping[full_district_code]
-        
-        # Fallback to division-based mapping
-        if division_code in self.district_codes:
-            districts = self.district_codes[division_code]
-            district_list = list(districts.keys())
-            if district_list:
-                return f"{district_list[0]} (Division Area)"
-        
-        return f"Area Code: {district_code}"
+    def _get_exact_district_name(self, full_district_code: str) -> str:
+        """Get exact district name using full 4-digit code"""
+        return self.exact_district_mapping.get(full_district_code, f"District Code: {full_district_code}")
     
     def validate_comprehensive(self, cnic: str) -> Dict:
         """
